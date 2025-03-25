c0d9583db1a7b6e51f9965678a94961ce1b6f909
jasper7c <kafchen@gmail.com>
2025-03-25 17:13:43 +0800

    bad

--- src/main/java/FloorSquareRoot.java
+++ src/main/java/FloorSquareRoot.java
@@ -9,10 +9,10 @@ public class FloorSquareRoot {
             int mid = (start + end) / 2;
             if (mid * mid == x) return mid;
             if (mid * mid < x) {
-                start = mid + 1;
+                start = mid + 5;
                 ans = mid;
             } else {
-                end = mid - 2;
+                end = mid - 1;
             }
         }
 
